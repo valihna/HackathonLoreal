2a1227eabbed6fe85f461e6fb47c19e8a88f4ef9
valihna <144777497+valihna@users.noreply.github.com>
2024-02-05 09:31:48 +0100

    Initialize JMZ_Inscription

---
+++ frontend/src/main.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import connexion from "./services/connexion";
+import App from "./App";
+import Candidats from "./pages/Candidats";
+import Histoire from "./pages/Histoire";
+import Concept from "./pages/Concept";
+import Votes from "./pages/Votes";
+import Login from "./pages/Login";
+import PageInscription from "./pages/PageInscription";
+import CardsAll from "./pages/CardsAll/CardsAll";
+import Home from "./pages/home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/histoire",
+        element: <Histoire />,
+      },
+      {
+        path: "/concept",
+        element: <Concept />,
+      },
+      {
+        path: "/candidats",
+        element: <Candidats />,
+      },
+      {
+        path: "/votes",
+        element: <Votes />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signin",
+        element: <PageInscription />,
+      },
+    ],
+  },
+  {
+    path: "/candidats",
+    element: <CardsAll />,
+    loader: () => {
+      return connexion
+        .get("/Candidats")
+        .then((response) => response.data)
+        .catch((err) => console.error(err));
+    },
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
