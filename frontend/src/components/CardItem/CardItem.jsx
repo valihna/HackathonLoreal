34b0e5b76e453c2ab4712cf598effd363c62c67a
valihna <144777497+valihna@users.noreply.github.com>
2024-02-05 09:31:51 +0100

    Initialize max_buttons

---
+++ frontend/src/components/CardItem/CardItem.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./carditem.css";
+
+function CardItem({ data }) {
+  return (
+    <div className="CardItem">
+      <Link className="CardItem_card" to={`${data.id}`}>
+        <img
+          className="CardItem_img"
+          src={data.photo}
+          alt="photographie"
+          style={{ cursor: "pointer" }}
+        />
+      </Link>
+      <div className="CardItem_text">
+        <h2 className="CardItem_h2">candidate numéro {data.id}</h2>
+        <h2 className="CardItem_h2">{data.name}</h2>
+        <h2 className="CardItem_h2">{data.prenom}</h2>
+        <h2 className="CardItem_h2">{data.age}</h2>
+        <div className="button">
+          <button type="button" className="greenButton">
+            Acceptée
+          </button>
+          <button type="button" className="redButton">
+            Refusée
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default CardItem;
