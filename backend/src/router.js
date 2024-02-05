e362ca77fa38fa1fb1b0857367df84b289a6d578
valihna <144777497+valihna@users.noreply.github.com>
2024-02-05 09:31:52 +0100

    Initialize sebastien

---
+++ backend/src/router.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const router = express.Router();
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, "public/assets/images");
+  },
+  filename(req, file, callback) {
+    callback(null, "monfichier.jpg");
+  },
+});
+
+const upload = multer({
+  storage,
+});
+
+// Define Your API Routes Here
+
+// Import itemControllers module for handling item-related operations
+const itemControllers = require("./controllers/itemControllers");
+const authControllers = require("./controllers/authControllers");
+const candidatControllers = require("./controllers/candidatControllers");
+// Route to get a list of items
+router.get("/items", itemControllers.browse);
+
+// Route to get a specific item by ID
+router.get("/items/:id", itemControllers.read);
+
+// Route to add a new item
+router.post("/items", itemControllers.add);
+router.post("/login", authControllers.log);
+router.post("/candidat", upload.single("image"), candidatControllers.add);
+
+router.get("/candidats", candidatControllers.browse);
+router.get("/candidats/:id", candidatControllers.read);
+
+router.get("/retenu", candidatControllers.retenu);
+
+module.exports = router;
---
+++ frontend/src/components/DisplayCardID/DisplayCardId.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import CardItemId from "../CardItemId/CardItemId";
+import "./displayCardId.css";
+
+function DisplayCardId({ basePath }) {
+  const items = useLoaderData();
+
+  return (
+    <div className="DisplayCardId">
+      {items.map((item) => (
+        <CardItemId key={item.id} data={item} basePath={basePath} />
+      ))}
+    </div>
+  );
+}
+
+export default DisplayCardId;
